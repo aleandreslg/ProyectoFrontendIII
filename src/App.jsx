9fe0b5ceccc1cbd4f56219f109033fd284133cea
Ceci <ceciliaaquinouy@gmail.com>
2022-12-08 18:11:11 -0300

    dark footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
-import React, {useContext} from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import styles from "./App.module.css"
-import Home from './Routes/Home';
-import Contact from './Routes/Contact';
-import Favs from './Routes/Favs';
-import Detail from './Routes/Detail';
+import React, { useContext } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import styles from "./App.module.css";
+import Home from "./Routes/Home";
+import Contact from "./Routes/Contact";
+import Favs from "./Routes/Favs";
+import Detail from "./Routes/Detail";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import { ContextGlobal } from "./Components/utils/global.context";
 
-
 function App() {
-  const { state, dispatch} = useContext(ContextGlobal)
+  const { state, dispatch } = useContext(ContextGlobal);
   return (
-    <div className="App" style={{backgroundColor:state.bgColor, color:state.ftColor, width: "100%", height: "100vh", minHeight: "100%"}}>  {/*Agregar esto!!!!!!!!*/}
+    <div
+      className="App"
+      style={{
+        backgroundColor: state.bgColor,
+        color: state.ftColor,
+        width: "100%",
+        minHeight: "100%",
+      }}
+    >
+      {" "}
+      {/*Agregar esto!!!!!!!!*/}
       {/* <h1>App</h1> */}
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/favs" element={<Favs/>}/>
-          <Route path="/odontologo/:id" element={<Detail/>}/>
-          <Route path="*" element={<Navigate to="/" />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/favs" element={<Favs />} />
+          <Route path="/odontologo/:id" element={<Detail />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,22 +1,74 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import logo from "./DH.png";
-import { ContextGlobal } from './utils/global.context'
+import { ContextGlobal } from "./utils/global.context";
 
 const Footer = () => {
-  const { state } = useContext(ContextGlobal)
+  const { state } = useContext(ContextGlobal);
   return (
-    <footer style={{display:"flex", justifyContent:"space-between", alignItems:"center", margin:"50px", width:"90vw"}}>
-      <div style={{display:"flex", alignItems:"center", marginLeft:"50px"}}>
+    <footer
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "50px",
+        width: "90vw",
+      }}
+    >
+      <div
+        style={{ display: "flex", alignItems: "center", marginLeft: "50px" }}
+      >
         <p>Powered by</p>
-        <img src={logo} alt="DH-logo" style={{ width: "300px", filter: state.bgFlag === "DARK" ? "invert(100%)" : null }} />
+        <img
+          src={logo}
+          alt="DH-logo"
+          style={{
+            width: "300px",
+            filter: state.bgFlag === "DARK" ? "invert(100%)" : null,
+          }}
+        />
       </div>
-      <div style={{width:"200px", display: "flex", justifyContent:"space-between", gap:"20px", alignItems:"center"}}>
-        <img src="../Images/ico-facebook.png" alt="facebook" style={{ width: "40px", filter: state.bgFlag === "DARK" ? null : "invert(100%)" }} />
-        <img src="../Images/ico-instagram.png" alt="instagram" style={{ width: "40px", filter:state.bgFlag === "DARK" ? null : "invert(100%)" }} />
-        <img src="../Images/ico-whatsapp.png" alt="whatsapp" style={{ width: "40px", filter:state.bgFlag === "DARK" ? null : "invert(100%)" }} />
-        <img src="../Images/ico-tiktok.png" alt="tiktok" style={{ width: "40px", filter: state.bgFlag === "DARK" ? null : "invert(100%)" }} />
+      <div
+        style={{
+          width: "200px",
+          display: "flex",
+          justifyContent: "space-between",
+          gap: "20px",
+          alignItems: "center",
+        }}
+      >
+        <img
+          src="../Images/ico-facebook.png"
+          alt="facebook"
+          style={{
+            width: "40px",
+            filter: state.bgFlag === "DARK" ? null : "invert(100%)",
+          }}
+        />
+        <img
+          src="../Images/ico-instagram.png"
+          alt="instagram"
+          style={{
+            width: "40px",
+            filter: state.bgFlag === "DARK" ? null : "invert(100%)",
+          }}
+        />
+        <img
+          src="../Images/ico-whatsapp.png"
+          alt="whatsapp"
+          style={{
+            width: "40px",
+            filter: state.bgFlag === "DARK" ? null : "invert(100%)",
+          }}
+        />
+        <img
+          src="../Images/ico-tiktok.png"
+          alt="tiktok"
+          style={{
+            width: "40px",
+            filter: state.bgFlag === "DARK" ? null : "invert(100%)",
+          }}
+        />
       </div>
-      
     </footer>
   );
 };
